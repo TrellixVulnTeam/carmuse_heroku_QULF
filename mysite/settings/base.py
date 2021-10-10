edf07ca4bfb966745d4a5420792cce4c7df75423
Yariv Friedman <yrfriedman@gmail.com>
2021-10-10 14:23:17 +0200

    deploy to do as dockerfile and sqlite

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -137,23 +137,23 @@ import dj_database_url
 #         "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
 #     }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dbcar',
-        'USER': 'caruser',
-        'PASSWORD': '2020',
-        'HOST': '',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'dbcar',
+#         'USER': 'caruser',
+#         'PASSWORD': '2020',
+#         'HOST': '',
+#         'PORT': '5432',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
